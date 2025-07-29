eb5cc1f6e854b781f1ccf0b9afa591390d9df221
Chidubem <emekaogbuchidubem@gmail.com>
2025-07-29 06:56:56 +0100

    slight changes

--- my-portfolio/astro.config.mjs
+++ my-portfolio/astro.config.mjs
@@ -7,5 +7,9 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
+  },
+  server: {
+    host: '172.20.10.2', // or use your IP like '192.168.1.100'
+    port: 5173
   }
 });
