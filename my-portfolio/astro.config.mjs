81679fc3acc6ffeacd8a68d9435b674464ab849a
Chidubem <emekaogbuchidubem@gmail.com>
2025-08-03 11:27:55 +0100

    domain name changes

--- my-portfolio/astro.config.mjs
+++ my-portfolio/astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dubem.vercel.app',
+  site: 'https://chidubem.name.ng',
 
   vite: {
     plugins: [tailwindcss()]
